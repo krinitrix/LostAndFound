774acc7ed2188eaad77a18a6c66d601e384ca8bc
Nithin Krishna <krishnanithinnk47@gmail.com>
2025-04-23 21:34:35 +0530

    databaseTest

---
+++ lostfound-portal/config/database.php
@@ -0,0 +1,24 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $username = "root";       
+    private $password = "";           
+    private $database = "LostAndFound";
+    public $conn;
+
+    public function connect() {
+        $this->conn = new mysqli(
+            $this->host,
+            $this->username,
+            $this->password,
+            $this->database
+        );
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+
+        return $this->conn;
+    }
+}
+?>
